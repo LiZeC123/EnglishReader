b81e530e5776ff85e9dfea043eb667e8b2736a13
LiZeC <tomgreem123@gmail.com>
2017-10-15 15:36:28 +0900

    完成基本读取

--- app/src/main/java/lizec/lizec/englishreader/MainActivity.java
+++ app/src/main/java/lizec/lizec/englishreader/MainActivity.java
@@ -10,31 +10,29 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import reader.WordGetter;
+import dataGetter.DataModel;
+import dataGetter.ModelGetter;
+import dataGetter.TranslatePair;
 
 public class MainActivity extends AppCompatActivity {
-    WordGetter wordGetter = new WordGetter();
-    WordGetter.WordTagPair thisPair;
-
+    TranslatePair thisPair;
+    DataModel thisModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File wordFileDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
-        if(!wordFileDir.exists()){
-            boolean b = wordFileDir.mkdir();
-            Log.d("mkdir",Boolean.toString(b));
-        }
+        //初始化文件夹
+        File dataSpaceDir = initDir();
 
-        File wordFile = new File(wordFileDir,"wordList.txt");
-        try {
-            wordGetter.readWord(wordFile);
-            thisPair = wordGetter.getRandomPair();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        if(isInitFiles(dataSpaceDir)){
+            initFiles(dataSpaceDir);
         }
+        thisModel = initModel(dataSpaceDir);
+        thisPair = thisModel.getPair();
+
 
         final TextView txtViewTag = (TextView) findViewById(R.id.txtViewTag);
         final TextView txtViewWord = (TextView) findViewById(R.id.txtViewWord);
@@ -44,17 +42,68 @@ public class MainActivity extends AppCompatActivity {
         btnTag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtViewTag.setText(thisPair.tag);
+                txtViewTag.setText(thisPair.Hide);
             }
         });
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                thisPair = wordGetter.getRandomPair();
+                thisPair = thisModel.getPair();
                 txtViewTag.setText("");
-                txtViewWord.setText(thisPair.word);
+                txtViewWord.setText(thisPair.Look);
             }
         });
+    }
+
+    private File initDir(){
+        File wordFileDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        assert wordFileDir != null;
+        if(!wordFileDir.exists()){
+            boolean b = wordFileDir.mkdir();
+            Log.d("初始化文件夹",Boolean.toString(b));
+        }
+        return wordFileDir;
+    }
+
+    private boolean isInitFiles(File dataSpaceDir){
+        File readSentence      = new File(dataSpaceDir,"readSentence.md");
+        File translateSentence = new File(dataSpaceDir,"translateSentence.md");
+        File translateWord     = new File(dataSpaceDir,"translateWord.md");
+
+        return !(readSentence.exists() && translateSentence.exists() && translateWord.exists());
+    }
+
+    private boolean initFiles(File dataSpaceDir){
+        File readSentence      = new File(dataSpaceDir,"readSentence.md");
+        File translateSentence = new File(dataSpaceDir,"translateSentence.md");
+        File translateWord     = new File(dataSpaceDir,"translateWord.md");
+
+        boolean isSuccess = false;
+        try{
+            isSuccess = readSentence.createNewFile();
+            isSuccess &= translateSentence.createNewFile();
+            isSuccess &= translateWord.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  isSuccess;
+    }
+
+    private DataModel initModel(File dataSpaceDir) {
+        File readSentence      = new File(dataSpaceDir,"readSentence.md");
+        //File translateSentence = new File(dataSpaceDir,"translateSentence.md");
+        //File translateWord     = new File(dataSpaceDir,"translateWord.md");
+
+        DataModel model;
+        try {
+            model = ModelGetter.mdToList(readSentence);
+        } catch (Exception e) {
+            model = new DataModel();
+            model.add(new TranslatePair("暂无数据","无本地解释"));
+        }
+
+        return model;
     }
 }
